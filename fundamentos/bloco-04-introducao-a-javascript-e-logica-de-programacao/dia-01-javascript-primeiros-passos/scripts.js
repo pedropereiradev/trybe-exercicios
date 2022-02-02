16cd21e793678940d1c38291f837118996eb8e40
Pedro Pereira <pedro.2108@hotmail.com>
2022-02-02 15:59:00 -0300

    Exemplo 2: Tipos Primitivos, Tipagem Dinâmica e Operações Aritméticas

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
@@ -1,11 +1,39 @@
+/* //Parte 1 - Variáveis 
 const myName = "Pedro";
 const birthCity = "Pelotas-RS";
 
 let birthYear = 2000;
 
 birthYear = 2030;
-/*birthCity = "São Paulo"; Exibe uma mensagem de erro pois estou tentando modificar o valor que foi atribuido a uma constante.  */
+//birthCity = "São Paulo"; Exibe uma mensagem de erro pois estou tentando modificar o valor que foi atribuido a uma constante.
 
 console.log(myName);
 console.log(birthCity);
-console.log(birthYear);
+console.log(birthYear); */
+
+// Parte 2 - Tipos primitivis, Tipagem Dinâmica e Operações Aritméticas
+/* let patientId = '50';
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+console.log(typeof patientAge); */
+
+const base = 5;
+let height = 8;
+
+let area;
+let perimeter;
+
+area = base * height;
+console.log(area);
+
+perimeter = (base*2) + (height*2);
+console.log(perimeter);
