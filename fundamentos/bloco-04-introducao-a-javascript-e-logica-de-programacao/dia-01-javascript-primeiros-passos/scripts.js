b13865e1db9c4118206668b55b13eec7a3b72757
Pedro Pereira <pedro.2108@hotmail.com>
2022-02-02 15:34:28 -0300

    Exemplo 1: Variáveis

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,11 @@
+const myName = "Pedro";
+const birthCity = "Pelotas-RS";
+
+let birthYear = 2000;
+
+birthYear = 2030;
+/*birthCity = "São Paulo"; Exibe uma mensagem de erro pois estou tentando modificar o valor que foi atribuido a uma constante.  */
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
