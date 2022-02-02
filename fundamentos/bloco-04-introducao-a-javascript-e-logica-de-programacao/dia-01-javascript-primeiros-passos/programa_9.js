4fd64348f49102adda6dca9bdf220969a5cf2e38
Pedro Pereira <pedro.2108@hotmail.com>
2022-02-02 19:27:42 -0300

    Finalizado programa 9

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/programa_9.js
@@ -0,0 +1,14 @@
+const firstAngle = 80;
+const secondAngle = 80;
+const thirdAngle = 20;
+let addAngle = 0;
+
+addAngle = firstAngle + secondAngle + thirdAngle;
+
+if (firstAngle <= 0 || secondAngle<=0 || thirdAngle<=0) {
+    console.log("Angulo inserido inválido");
+} else if (addAngle == 180) {
+    console.log(addAngle == 180);
+} else {
+    console.log(addAngle == 180);
+}
