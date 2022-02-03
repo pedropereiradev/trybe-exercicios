52746ed34c2192ca4099441d4c3827e5bf4d8cf4
Pedro Pereira <pedro.2108@hotmail.com>
2022-02-03 18:47:47 -0300

    Finalizado exercício 9

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicio_9.js
@@ -0,0 +1,9 @@
+array = [];
+
+for (let index = 0; index < 25; index += 1) {
+  array.push(index + 1);
+}
+
+for (let index2 = 0; index2 < array.length; index2 += 1) {
+  console.log((array[index2]) / 2);
+}
