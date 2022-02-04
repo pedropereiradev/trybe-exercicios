ed1dbc426b4c19658bbb1a055eb3be676d38e080
Pedro Pereira <pedro.2108@hotmail.com>
2022-02-04 17:56:04 -0300

    Adicionado arquivo ainda incompleto  desenvolvido na dinâmica da aula ao vivo

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -0,0 +1,60 @@
+// 1 - 
+// Faça um algoritmo que calcule a soma de 1 a 50 usando a estrutura "for" e retorne no formato:
+//A soma total de 1 a 50 é:
+
+/* let soma = 0;
+
+for (let index = 1; index <= 50; index += 1) {
+  soma += index;
+}
+console.log("O resultado da soma é: " + soma);
+ */
+
+//2 -
+
+// Crie um algoritmo que conte quantos números do intervalo entre 2 e 150 são divisíveis por 3.
+// Caso a quantidade seja igual a 50, exiba uma mensagem secreta.
+
+/* let quantidade = 0;
+
+for (let index = 2; index <= 150; index += 1) {
+  if ((index % 3) === 0) {
+    quantidade += 1;
+  }
+}
+
+console.log(quantidade);
+
+if (quantidade === 50) {
+  console.log("Vamo que Vamo!!");
+}
+ */
+
+
+// 3 -
+//Crie um algoritmo que simula o jogo "pedra, papel e tesoura" levando em consideração que são apenas duas pessoas jogando e imprima o resultado no formato:
+// "Player 1 won" ou "A Ties" ou "Player 2 won". 
+let player1 = "";
+let player2 = "";
+
+if (
+  (player1 === "pedra" && player2 === "pedra") ||
+  (player1 === "papel" && player2 === "papel") ||
+  (player1 === "tesoura" && player2 === "tesoura")
+) {
+  
+}
+
+
+
+//4
+//Desenvolva um algoritmo que verifica se a pessoa é maior ou menor de idade.
+//Imprima no console seguindo o exemplo: "A pessoa é maior de idade".
+//Bônus: Crie a condição utilizando operador ternário.
+
+
+
+// 5 
+
+//Crie um algoritmo que recebe a idade de Carolzita , Murilo e Baêta e imprime quem é a pessoa mais nova no formato:
+// "Pessoa" é a mais nova.
