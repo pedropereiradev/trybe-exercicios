e0770b317a229140b272a09f3ea05ed415805889
Pedro Pereira <pedro.2108@hotmail.com>
2022-02-07 11:06:29 -0300

    Finalizado exercícios da aula ao vivo

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -34,16 +34,27 @@ if (quantidade === 50) {
 // 3 -
 //Crie um algoritmo que simula o jogo "pedra, papel e tesoura" levando em consideração que são apenas duas pessoas jogando e imprima o resultado no formato:
 // "Player 1 won" ou "A Ties" ou "Player 2 won". 
-let player1 = "";
-let player2 = "";
 
-if (
-  (player1 === "pedra" && player2 === "pedra") ||
-  (player1 === "papel" && player2 === "papel") ||
-  (player1 === "tesoura" && player2 === "tesoura")
-) {
-  
-}
+/* let player1 = "pedra";
+let player2 = "pedra";
+
+if (player1 === player2) {
+  console.log('A Ties');
+} else if (
+  (player1 === "pedra" && player2 === "tesoura") ||
+  (player1 === "papel" && player2 === "pedra") ||
+  (player1 === "tesoura" && player2 === "papel")
+)
+{
+  console.log('Player 1 won');
+} else if (
+  (player1 === "tesora" && player2 === "pedra") ||
+  (player1 === "pedra" && player2 === "papel") ||
+  (player1 === "papel" && player2 === "tesoura")
+)
+{
+  console.log("Player 2 won");
+} */
 
 
 
@@ -52,9 +63,27 @@ if (
 //Imprima no console seguindo o exemplo: "A pessoa é maior de idade".
 //Bônus: Crie a condição utilizando operador ternário.
 
+/* 
+let idade = 28;
 
+console.log(idade >= 18 ? 'A pessoa é maior de idade' : 'A pessoa não é maior de idade');
+ */
 
 // 5 
 
 //Crie um algoritmo que recebe a idade de Carolzita , Murilo e Baêta e imprime quem é a pessoa mais nova no formato:
 // "Pessoa" é a mais nova.
+
+let idadeCarolzita = 20;
+let idadeMurilo = 25;
+let idadeBaeta = 30;
+
+if (idadeCarolzita < idadeMurilo && idadeCarolzita < idadeBaeta) {
+  console.log('Carolzita é a mais nova');
+} else if (idadeMurilo < idadeCarolzita && idadeMurilo < idadeBaeta) {
+  console.log('Murilo é o mais novo');
+} else if (idadeBaeta < idadeCarolzita && idadeBaeta < idadeMurilo) {
+  console.log('Baêta é a mais nova');
+} else {
+  console.log('Pessoas com idades iguais');
+}
