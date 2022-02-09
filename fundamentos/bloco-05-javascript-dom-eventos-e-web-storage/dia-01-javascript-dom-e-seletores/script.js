21914d499d3c0c20c5f9f445fc2c097766408668
Pedro Pereira <pedro.2108@hotmail.com>
2022-02-09 18:25:37 -0300

    Realizado atividade 1

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
@@ -7,6 +7,16 @@
 */
 
 // 1. Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
+function changeTagText(tagText, index, content) {
+  let newDescription = document.getElementsByTagName(tagText);
+  newDescription[index].innerText = content;
+}
+
+changeTagText(
+  "p",
+  1,
+  "Daqui a 2 anos serei um desenvolvedor pleno trabalhando no exterior ganhando em uma moeda forte."
+);
 
 
 // 2. Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
