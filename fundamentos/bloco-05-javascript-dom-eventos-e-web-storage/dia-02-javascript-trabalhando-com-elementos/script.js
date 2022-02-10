5a7e591872caed28e22cd00eda5fe0032ec4f5fb
Pedro Pereira <pedro.2108@hotmail.com>
2022-02-10 19:27:17 -0300

    Realizado requisito 2

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/script.js
@@ -3,3 +3,8 @@ let addTitle = document.createElement("h1");
 addTitle.innerText = "Exercício 5.2 - JavaScript DOM como filho da tag body";
 document.body.appendChild(addTitle);
 
+// 2. Adicione a tag main com a classe main-content como filho da tag body
+let addMain = document.createElement('main');
+addMain.className = 'main-content'
+document.body.appendChild(addMain);
+
