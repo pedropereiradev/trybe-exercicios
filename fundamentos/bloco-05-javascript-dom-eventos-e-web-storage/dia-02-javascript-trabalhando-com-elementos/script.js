4375a6b7f18bfaa6da6a58f61bff524a73f4c823
Pedro Pereira <pedro.2108@hotmail.com>
2022-02-10 19:25:56 -0300

    Realizado requisito 1

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/script.js
@@ -0,0 +1,5 @@
+// 1. Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body
+let addTitle = document.createElement("h1");
+addTitle.innerText = "Exercício 5.2 - JavaScript DOM como filho da tag body";
+document.body.appendChild(addTitle);
+
