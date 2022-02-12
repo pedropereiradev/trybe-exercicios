f3f5e81b769ef26c9f0bc27db05744ca3cb48a95
Pedro Pereira <pedro.2108@hotmail.com>
2022-02-11 21:39:30 -0300

    Finalizado todos os requisitos

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/exemplo-aula/main.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/exemplo-aula/main.js
@@ -51,24 +51,30 @@ inputText.addEventListener('keyup', changeText);
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
 // https://www.codexworld.com/how-to/open-url-in-new-browser-window-tab-javascript/#:~:text=If%20you%20want%20to%20open,the%20second%20parameter%20of%20window.
- let mySpotrybefy = document.querySelector("#my-spotrybefy");
+let mySpotrybefy = document.querySelector("#my-spotrybefy");
 mySpotrybefy.addEventListener('dblclick', function () {
   window.open('https://github.com/pedropereiradev', '_blank');
 });
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+mySpotrybefy.addEventListener('mouseover', function () {
+  mySpotrybefy.style.color = "#2fc18c";
+})
+mySpotrybefy.addEventListener('mouseleave', function () {
+  mySpotrybefy.style.color = "white";
+})
 
 // Segue abaixo um exemplo do uso de event.target:
 
-function resetText() {
+/* function resetText() {
   // O Event é passado como um parâmetro para a função.
   event.target.innerText = "Opção reiniciada";
   // O event possui várias propriedades, porém a mais usada é o event.target,
   // que retorna o objeto que disparou o evento.
 }
 
-firstLi.addEventListener("dblclick", resetText);
+firstLi.addEventListener("dblclick", resetText); */
 
 // Não precisa passar o parâmetro dentro da callback resetText. O próprio
 // navegador fará esse trabalho por você, não é legal? Desse jeito, o
