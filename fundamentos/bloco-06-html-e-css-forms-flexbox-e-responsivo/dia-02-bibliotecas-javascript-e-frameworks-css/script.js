bb057942d50a7221e9376bbc7abdc9c78bcf52aa
Pedro Pereira <pedro.2108@hotmail.com>
2022-02-22 10:59:40 -0300

    validação data com biblioteca DAtePickerX

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javascript-e-frameworks-css/script.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javascript-e-frameworks-css/script.js
@@ -1,1 +1,9 @@
-var picker = new Pikaday({ field: document.getElementById("datepicker") });
+// https://www.horadecodar.com.br/2021/04/03/como-pegar-a-data-atual-com-javascript/
+
+const data = document.getElementById("myInputId").DatePickerX.init({
+  mondayFirst: false,
+  minDate: new Date(),
+  format: 'dd/mm/yyyy'
+});
+
+
