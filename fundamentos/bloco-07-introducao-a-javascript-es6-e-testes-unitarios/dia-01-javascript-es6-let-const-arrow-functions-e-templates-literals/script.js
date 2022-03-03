23f1c4399cad6b443dc20b2e091947ef3b74d2be
Pedro Pereira <pedro.2108@hotmail.com>
2022-03-03 18:34:53 -0300

    exercicio 2

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-templates-literals/script.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-templates-literals/script.js
@@ -1,5 +1,5 @@
 // Exercício 1:
-const testingScope = escopo => {
+const testingScope = (escopo) => {
   if (escopo === true) {
     let ifScope = "Não devo ser utilizada fora do meu escopo (if)";
     console.log(`${ifScope} ótimo, fui utilizada no escopo !`);
@@ -7,6 +7,16 @@ const testingScope = escopo => {
     let elseScope = "Não devo ser utilizada fora meu escopo (else)";
     console.log(elseScope);
   }
-}
+};
 
 testingScope(true);
+
+// Exercício 2:
+// REFERENCIA:
+// https://ricardo-reis.medium.com/o-m%C3%A9todo-sort-do-array-javascript-482576734e0a
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// Seu código aqui.
+const orderArray = (oddsAndEvens) => oddsAndEvens.sort((a, b) => a - b);
+
+console.log(orderArray(oddsAndEvens)); // será necessário alterar essa linha 😉
