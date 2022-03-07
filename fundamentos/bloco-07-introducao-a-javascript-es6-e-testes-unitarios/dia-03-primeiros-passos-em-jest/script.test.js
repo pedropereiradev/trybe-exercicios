54eacdbe0856c6ef8da5a0d78c757bff599ca119
Pedro Pereira <pedro.2108@hotmail.com>
2022-03-07 18:36:23 -0300

    Realiza teste 2

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/script.test.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/script.test.js
@@ -4,4 +4,8 @@ describe("The sum function", () => {
   it("adds 4 + 5 to equal 9", () => {
     expect(sum(4, 5)).toBe(9);
   });
+  
+  it("adds 0 + 0 to equal 0", () => {
+    expect(sum(0, 0)).toBe(0);
+  });
 });
