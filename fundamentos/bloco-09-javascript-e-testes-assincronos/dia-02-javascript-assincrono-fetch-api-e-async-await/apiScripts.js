7b5c3ef28d1ad23a889589a56c4dbd2cbe60c883
Pedro Pereira <pedro.2108@hotmail.com>
2022-03-22 18:32:01 -0300

    Adiciona exercicio 1

--- fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-async-await/apiScripts.js
+++ fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-async-await/apiScripts.js
@@ -1,8 +1,21 @@
 // apiScript.js
 const API_URL = 'https://icanhazdadjoke.com/';
 
+const append = (joke) => {
+  const jokeContainer = document.querySelector('#jokeContainer');
+
+  jokeContainer.innerText = joke;
+}
+
 const fetchJoke = () => {
-  // Adicionar lógica aqui!
+  const myObject = {
+    method: 'GET',
+    headers: { Accept: 'application/json' },
+  };
+
+  fetch(API_URL, myObject)
+    .then((response) => response.json())
+    .then((data) => append(data.joke));
 };
 
 window.onload = () => fetchJoke();
